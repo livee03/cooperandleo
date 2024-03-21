8bc975ab3fbf0a3055693da1a34ed047be82deea
livee03 <olivia.harbourne@edu.sait.ca>
2024-03-21 12:49:02 -0600

    heyyo

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+    accordionItems.forEach(item => {
+        item.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    });
+});
+
+
